83b8b22135517b098c5d4408d00b8ac6b4b29147
Paweł Dąbrowski <512@keemail.me>
2025-04-07 23:30:07 +0200

    Add showcase video

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,10 @@
+import "@vidstack/react/player/styles/default/theme.css";
+import "@vidstack/react/player/styles/default/layouts/video.css";
+
 import { Stack, Text, Highlight } from "@chakra-ui/react";
 import { FAQ } from "./FAQ";
+import { MediaPlayer, MediaProvider, Poster, Track } from "@vidstack/react";
+import { defaultLayoutIcons, DefaultVideoLayout } from "@vidstack/react/player/layouts/default";
 
 export function HomePage() {
   return (
@@ -11,6 +16,27 @@ export function HomePage() {
         </Highlight>{" "}
         a free tool to protect your writing against copyright theft.
       </Text>
+      <MediaPlayer
+        playsInline
+        viewType="video"
+        streamType="on-demand"
+        title="Invisible Text Watermarks Showcase"
+        src="/Invisible-Text-Watermarks/Invisible_Text_Watermarks_showcase.mp4"
+        poster="/Invisible-Text-Watermarks/Invisible_Text_Watermarks_showcase_thumbnail.webp"
+        className="player"
+        style={{ backgroundColor: "var(--chakra-colors-bg-emphasized)" }}
+      >
+        <Poster className="vds-poster" />
+        <Track
+          src="/Invisible-Text-Watermarks/Invisible_Text_Watermarks_showcase_subtitles.vtt"
+          kind="subtitles"
+          label="English"
+          lang="en-US"
+          default
+        />
+        <MediaProvider />
+        <DefaultVideoLayout icons={defaultLayoutIcons} />
+      </MediaPlayer>
       <FAQ />
     </Stack>
   );
